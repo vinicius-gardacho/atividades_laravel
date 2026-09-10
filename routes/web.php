cc6ae1228ee8e2af045ce86013ab15d0a1547cb8
vinicius <engs-viniciusgardacho@camporeal.edu.br>
2026-09-09 21:26:42 -0300

    Atividade 1

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/sobre', function () {
+    return 'Sobre';
+});
+
+Route::get('/alunos', function () {
+    return 'Alunos';
+});
+
+Route::get('/contato', function () {
+    return 'Contato';
+});
