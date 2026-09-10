e2e54c9b623d3c9243a09908f982b6593c42cc88
vinicius <engs-viniciusgardacho@camporeal.edu.br>
2026-09-09 22:29:37 -0300

    Atividade 18

--- routes/web.php
+++ routes/web.php
@@ -1,30 +1,20 @@
 <?php
 
-use App\Http\Controllers\AlunoController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/sobre', function () {
-    return 'Sobre';
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-Route::resource('alunos', AlunoController::class);
-
-Route::get('/contato', function () {
-    return 'Contato';
-});
-
-Route::get('/produto/{id}', function ($id) {
-    return "Produto: $id";
-});
-
-Route::get('/categoria/{id}', function ($id) {
-    return "Categoria: $id";
-});
-
-Route::get('/usuario/{id}', function ($id) {
-    return "Usuário: $id";
-});
+require __DIR__.'/auth.php';
